ad41a605c921deb861ae288eebc3de66d97fad67
404djl <3490002122@qq.com>
2025-04-02 14:53:36 +0800

    finish lab2 trace

--- kernel/proc.c
+++ kernel/proc.c
@@ -146,6 +146,8 @@ found:
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
 
+  p->kama_syscall_trace = 0;
+
   return p;
 }
 
@@ -321,6 +323,8 @@ fork(void)
   acquire(&np->lock);
   np->state = RUNNABLE;
   release(&np->lock);
+
+  np->kama_syscall_trace = p->kama_syscall_trace;
 
   return pid;
 }
